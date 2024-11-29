11a1af5ca8d7ef1f29a31a81965d3e00c82c8889
Aksharasimariya <aksharasimariya9@gmail.com>
2024-11-29 15:36:43 +0530

    project1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  extend: {
+    screens: {
+      "xs": '400px'
+      // => @media (min-width: 992px) { ... }
+             },
+         }
+        }  
